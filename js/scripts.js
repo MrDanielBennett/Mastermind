5f3f5e5fe844bec76861ae3d7b456ea959a8f93b
Jared Reando and Kristin Brewer-Lowe <kristin@>
2018-11-05 14:35:06 -0800

    Add function to calculate color match.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
-// // Business Logic
-//
-// function Mastermind() {
-//   // turnCount = 0;
-//   // This is going to count the number of turns a player makes.
-//   // playerGuess
-//   // This will store the player's guess that's input in the UI.
-//   // call master configuration.
-// }
-//
+// Business Logic
+var mastermind = new Mastermind();
+
+function Mastermind() {
+  // turnCount = 0;
+  // This is going to count the number of turns a player makes.
+  this.playerGuess = ['red', 'blue', 'green', 'yellow'];
+  this.tempPlayerGuess = [];
+  this.masterConfig = ['red', 'yellow', 'green', 'blue'];
+  // This will store the player's guess that's input in the UI.
+  // call master configuration.
+}
+
 // Mastermind.prototype.checkForWin() = function(){
 //   // Evaluate the player guess against the master config.
 // }
-//
-// Mastermind.prototype.pegResult() = function(){
-//   // Evaluate the player guess against master config and return # of matching colors and return # of matching color + position.
-// }
-//
-// Mastermind.prototype.currentTurn() = function(){
+
+Mastermind.prototype.pegResult = function(){
+  console.log(exactMatch(this.playerGuess, this.masterConfig, this.tempPlayerGuess));
+  console.log(colorMatch(this.tempPlayerGuess, this.masterConfig));
+}
+
+// Mastermind.prototype.currentTurn = function(){
 //   // Read current Mastermind turn count and add 1.
 // }
 //
@@ -24,60 +28,44 @@
 //   // masterConfig = the method to generate the random config.
 // }
 //
-// MasterConfiguration.prototype.generateMaster() = function(){
+// MasterConfiguration.prototype.generateMaster = function(){
 //   // This will generate and return a random set of 4 colors (from a selection of 6 colors).
 // }
-//
-// // User Interface Logic
-// $(document).ready(function(){
-//
-// });
 
-var guess = ['yellow', 'blue', 'green', 'purple'];
-var masterConfig = ['yellow', 'blue', 'green', 'purple'];
+function exactMatch(array1, array2, tempPlayerGuess)  {
 
-
-var correctColors = 0;
-
-var testArray = [];
-
-function exactMatch(array1, array2)  {
-  testArray = [];
   var exactMatch = 0;
+
   if(array1.length != array2.length) {
     console.log("Those arrays are different lengths");
   } else {
     for(var i = 0; i < array1.length; i++) {
       if(array1[i] === array2[i]) {
-        console.log("Position " + i + " match");
         exactMatch += 1;
-        testArray.push("match");
+        tempPlayerGuess.push("match");
       } else {
-        console.log("No match. Match broken at index " + i);
-        testArray.push(array1[i]);
+        tempPlayerGuess.push(array1[i]);
       }
     }
-    console.log("You have " + exactMatch + " match perfectly.");
-    console.log("Your test array reads: " + testArray);
   }
   return exactMatch;
 }
 
 function colorMatch(array1, array2) {
-  var tempMatch = 0;
+  var colorMatch = 0;
+  var tempMasterArray = array2.slice();
 
-  array1.forEach(function(index) {
-
-    for(var i = 0; i < array2.length; i++) {
-      if(index === array2[i]) {
-        tempMatch += 1;
-
-        console.log("i = " + i);
-        console.log(index);
-      }
+  array1.forEach(function(position) {
+    if(tempMasterArray.indexOf(position) != -1) {
+      var splicePoint = tempMasterArray.indexOf(position);
+      colorMatch += 1;
+      tempMasterArray.splice(splicePoint,1,'colormatch');
     }
-  });
-  console.log(tempMatch);
-  console.log("Your test array reads: " + testArray);
-  console.log("You match the colors of " + tempMatch + " tiles.");
-};
+   });
+   return colorMatch;
+}
+
+// User Interface Logic
+$(document).ready(function(){
+
+});
