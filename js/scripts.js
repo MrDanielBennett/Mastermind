a8acb4fdb553ef8419cc3e571a122047093c2034
Jared Reando and Kristin Brewer-Lowe <jared@>
2018-11-05 10:43:44 -0800

    Add business logic outline

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
 // Business Logic
 
+function Mastermind() {
+  // turnCount = 0;
+  // This is going to count the number of turns a player makes.
+  // playerGuess
+  // This will store the player's guess that's input in the UI.
+  // call master configuration.
+}
+
+Mastermind.prototype.checkForWin() = function(){
+  // Evaluate the player guess against the master config.
+}
+
+Mastermind.prototype.pegResult() = function(){
+  // Evaluate the player guess against master config and return # of matching colors and return # of matching color + position.
+}
+
+Mastermind.prototype.currentTurn() = function(){
+  // Read current Mastermind turn count and add 1.
+}
+
+function MasterConfiguration() {
+  // masterConfig = the method to generate the random config.
+}
+
+MasterConfiguration.prototype.generateMaster() = function(){
+  // This will generate and return a random set of 4 colors (from a selection of 6 colors).
+}
+
 // User Interface Logic
 $(document).ready(function(){
-  $("h1").click(function(){
-    $("h1".toggle();
-  )};
+
 });
